911e61c13bf52c506c25aa026f19d1cadf495666
Benji Marshall <benji.john.marshall@gmail.com>
2013-06-01 15:30:21 +0100

    Build: 01/06/2013 15:30

--- commit.bat
+++ commit.bat
@@ -1,8 +1,11 @@
 @date /t > tmp.tmp
 @set /p thedate= < tmp.tmp
 @del tmp.tmp
+@time /t > tmp.tmp
+@set /p thetime= < tmp.tmp
+@del tmp.tmp
 @set buildstr=Build: 
-@set full=%buildstr%%thedate%
+@set full=%buildstr%%thedate%%thetime%
 @git add *
 @git commit -m "%full%"
 @PAUSE
